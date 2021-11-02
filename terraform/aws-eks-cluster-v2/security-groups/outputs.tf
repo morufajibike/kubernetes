7524f14ea96db534cb9b47cd9d054501b34758b0
Moruf Ajibike <abiodun.ajibike1@yahoo.com>
2021-11-02 21:11:52 +0000

    Refactoring

---
+++ terraform/aws-eks-cluster-v2/security-groups/outputs.tf
@@ -0,0 +1,7 @@
+output "worker_group_mgmt_sg_one_id" {
+  value = aws_security_group.worker_group_mgmt_one.id
+}
+
+output "worker_group_mgmt_sg_two_id" {
+  value = aws_security_group.worker_group_mgmt_two.id
+}
