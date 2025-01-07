f195a18e319e715c125747f2b2313ddcfa7f00d5
Moruf Ajibike <abiodun.ajibike1@yahoo.com>
2025-01-07 21:59:23 +0000

    Add ECR repo

--- terraform/aws-eks-cluster-v3/main.tf
+++ terraform/aws-eks-cluster-v3/main.tf
@@ -8,6 +8,15 @@ resource "random_string" "suffix" {
   special = false
 }
 
+resource "aws_ecr_repository" "repo" {
+  name                 = "django-app"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
 ########## SECURITY GROUPS ##########
 module "sgs" {
   source = "./security-groups"
