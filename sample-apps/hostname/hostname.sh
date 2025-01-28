e1cfcc157afe962242fa1fd8859296804ea4622e
Moruf Ajibike <abiodun.ajibike1@yahoo.com>
2025-01-28 17:36:36 +0000

    Add GitHub action workflow

--- sample-apps/hostname/hostname.sh
+++ sample-apps/hostname/hostname.sh
@@ -4,14 +4,21 @@
 
 HOSTNAME=`hostname`
 VERSION=${VERSION:-v1}
+IMAGE_TAG=${IMAGE_TAG:-nginx:alpine}
+
 cat > /usr/share/nginx/html/index.html <<EOF
 <HTML>
 <HEAD>
-<TITLE>This page is on $HOSTNAME and is version $VERSION</TITLE>
-</HEAD><BODY>
-<H1>THIS IS HOST $HOSTNAME</H1>
-<H2>And we're running version: $VERSION</H2>
-</BODY>
+   <body>
+       <h1>Welcome to the Kubernetes Deployment Test Page</h1>
+       <p>This page is on $HOSTNAME and is version $VERSION</p>
+       <p>Hostname: $HOSTNAME</p>
+       <p>And we're running version: $VERSION</p>
+       <p>Current Date and Time: $(date)</p>
+       <p>Image Tag: $IMAGE_TAG</p>
+   </body>
+
+
 </HTML>
 EOF
 
